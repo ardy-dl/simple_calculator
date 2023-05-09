71d578df7b2156724bfe5918d1a66965977643c8
unknown <rosedenisejdeleon@iskolarngbayan.pup.edu.ph>
2023-05-09 18:37:29 +0800

    addition is working

--- calculator.py
+++ calculator.py
@@ -12,13 +12,19 @@ def division(num1, num2):
     return num1 / num2
 
 # ask the user for operation
+operation = input("Select an operation (+, -, *, /): ")
+
 # ask for two numbers
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
+# process the inputs
+if operation == "+":
+    print(num1, "+", num2, "=", addition(num1,num2))
+else:
+    print("Invalid operation")
 # display result
 # ask if want to try again
 # if yes, repeat from step 1
 # else, print "thank you" and exit the program
 # design UI
 
-print(addition(num1,num2))
