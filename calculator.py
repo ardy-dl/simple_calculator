2273d02ef634c2e646bc8c42a63fa82b6fa375f8
unknown <rosedenisejdeleon@iskolarngbayan.pup.edu.ph>
2023-05-09 18:32:04 +0800

    method not working

---
+++ calculator.py
@@ -0,0 +1,24 @@
+# create method
+def addition(num1, num2):
+    num1 + num2
+
+def subtraction(num1, num2):
+    num1 - num2
+
+def multiplication(num1, num2):
+    num1 * num2
+
+def division(num1, num2):
+    num1 / num2
+
+# ask the user for operation
+# ask for two numbers
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+# display result
+# ask if want to try again
+# if yes, repeat from step 1
+# else, print "thank you" and exit the program
+# design UI
+
+print(addition(num1,num2))
