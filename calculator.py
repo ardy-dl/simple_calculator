2136e59b65aa51aa2f6cb23bee0047cc0dafc1a5
unknown <rosedenisejdeleon@iskolarngbayan.pup.edu.ph>
2023-05-09 18:33:32 +0800

    method now working

--- calculator.py
+++ calculator.py
@@ -1,15 +1,15 @@
 # create method
 def addition(num1, num2):
-    num1 + num2
+    return num1 + num2
 
 def subtraction(num1, num2):
-    num1 - num2
+    return num1 - num2
 
 def multiplication(num1, num2):
-    num1 * num2
+    return num1 * num2
 
 def division(num1, num2):
-    num1 / num2
+    return num1 / num2
 
 # ask the user for operation
 # ask for two numbers
