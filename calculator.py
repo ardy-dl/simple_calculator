82ce10f0568a4d121e0a4d483ad24acecf601580
unknown <rosedenisejdeleon@iskolarngbayan.pup.edu.ph>
2023-05-10 09:47:43 +0800

    Tweak lots of errors

--- calculator.py
+++ calculator.py
@@ -13,24 +13,30 @@ def division(num1, num2):
 
 # ask the user for operation
 operation = input("Select an operation (+, -, *, /): ")
-
 # ask for two numbers
-num1 = float(input("Enter the first number: "))
-num2 = float(input("Enter the second number: "))
-# process the inputs
-if operation == "+":
-    print(num1, "+", num2, "=", addition(num1,num2))
-elif operation == "-":
-    print(num1, "-", num2, "=", subtraction(num1,num2))
-elif operation == "*":
-    print(num1, "+", num2, "=", addition(num1,num2))
-elif operation == "/":
-    print(num1, "+", num2, "=", addition(num1,num2))
-else:
-    print("Invalid operation")
+try:
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+except ValueError:
+    print("Sorry! Only numbers are accepted.")
 # display result
+else:
+    if operation == "+":
+        print(num1, "+", num2, "=", addition(num1,num2))
+    elif operation == "-":
+        print(num1, "-", num2, "=", subtraction(num1,num2))
+    elif operation == "*":
+        print(num1, "*", num2, "=", multiplication(num1,num2))
+    elif operation == "/":
+        print(num1, "/", num2, "=", division(num1,num2))
+    else:
+        print("Invalid operation")
+
+
 # ask if want to try again
+
 # if yes, repeat from step 1
+
+
 # else, print "thank you" and exit the program
 # design UI
-
